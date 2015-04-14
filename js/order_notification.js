b4a6bb97a01771c51e060c1285c4fa277111d937
wonder32 <s.schotvanger@gmail.com>
2015-04-14 22:01:19 +0200

    notifications

---
+++ js/order_notification.js
@@ -0,0 +1,84 @@
+jQuery(document).ready(function($){
+
+	var playing = false;
+
+	var cur_ord = window.localStorage.getItem('last_order');
+	
+	var first_order = $('#the-list tr').first().attr('id').replace('post-', '');
+
+	if(first_order != undefined && cur_ord != null && first_order > cur_ord){
+		var diff = (first_order - cur_ord);
+		$('embed').remove();
+		$('body').append('<audio id="notification" src="'+notification.sound+'" preload="auto" loop="true"></audio>');
+		playing = true;
+		var sound = document.getElementById('notification');
+		sound.play();
+		window.localStorage.setItem('last_order', first_order);
+		append_button();
+	}
+	
+	if(cur_ord == null){
+		window.localStorage.setItem('last_order', first_order);
+	}
+	
+	if(!playing){
+		setTimeout(function(){
+			window.location.reload();
+		}, 30000);
+	}
+	
+	function append_button(){
+		$('.subsubsub').before(
+				"<div class='notification_container'>" +
+					"<a class='button button-primary button-hero button-notification' href='#' id='stop_notification'><strong>Stop and show order!</strong></a>" +
+				"</div>"+
+                                "<div class='notification_container'>" +
+					"<a class='button button-primary button-hero button-notification' href='#' id='stop_and_print'><strong>Stop and print!</strong></a>" +
+				"</div>"
+                                
+			);
+		
+		$('.wrap').on('click', '#stop_notification', function(){
+			if(sound != undefined){
+				sound.pause();
+			}
+			
+			if(diff != undefined && diff > 0){
+				
+				var last_order = window.localStorage.getItem('last_order');
+				
+				for(var i = 0; i < diff; i++){
+					var order_url = $('#the-list tr[id="post-'+(last_order - i)+'"] td[class^="order_title"]').find('a').first().attr('href');
+					window.open(order_url);  
+				}
+			}
+			
+			$(this).fadeOut('slow', function(){
+				window.location.reload();
+			});
+			
+		});
+                $('.wrap').on('click', '#stop_and_print', function(){
+			if(sound != undefined){
+				sound.pause();
+			}
+			
+			if(diff != undefined && diff > 0){
+				
+				var last_order = window.localStorage.getItem('last_order');
+				
+				for(var i = 0; i < diff; i++){
+					var order_url_print = $('#the-list tr[id="post-'+(last_order - i)+'"] td[class^="order_actions"] a[class^="print-preview-button"]').first().attr('href');
+					window.open(order_url_print);  
+				}
+			}
+			
+			$(this).fadeOut('slow', function(){
+				window.location.reload();
+			});
+			
+		});
+	}
+	
+});
+
--- woocommerce_notification.php
+++ woocommerce_notification.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Woocommerce Notofication
+Plugin Name: Woocommerce Notification
 Plugin URI: http://www.puddinq.mobi
-Description: Get sound notofication in order view when order is made
+Description: Get sound notification in order view when order is made
 Version: 0.0.1
 Author: Wonder32
 Author URI: www.puddinq.mobi/wip/stefan-schotvanger
@@ -10,26 +10,26 @@ Author URI: www.puddinq.mobi/wip/stefan-schotvanger
 
 if(in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 
-	if (!class_exists( 'woocommerce_order_alarm' ) ) {
+	if (!class_exists( 'woocommerce_notification' ) ) {
 		
-		class woocommerce_order_alarm{
+		class woocommerce_notification{
 			
-			private $alarm;
+			private $notification;
 			
 			public function __construct(){
-				add_action('manage_shop_order_posts_custom_column', array($this, 'order_alarm'), 10, 2);
+				add_action('manage_shop_order_posts_custom_column', array($this, 'order_notification'), 10, 2);
 			}
 			
-			public function order_alarm(){
-				if(empty($this->alarm)){
-					$this->alarm = plugin_dir_url( __FILE__ ) .'alarm.mp3';
-					wp_enqueue_script('order_alarm_js', plugin_dir_url( __FILE__ ) . '/js/order_alarm.js', array('jquery') );
-					wp_enqueue_style('order_alarm_css',  plugin_dir_url( __FILE__ ) . '/css/order_alarm.css' );
-					wp_localize_script('order_alarm_js', 'alarm', array('sound' => $this->alarm));
+			public function order_notification(){
+				if(empty($this->notification)){
+					$this->notification = plugin_dir_url( __FILE__ ) .'notification.mp3';
+					wp_enqueue_script('order_notification_js', plugin_dir_url( __FILE__ ) . '/js/order_notification.js', array('jquery') );
+					wp_enqueue_style('order_notification_css',  plugin_dir_url( __FILE__ ) . '/css/order_notification.css' );
+					wp_localize_script('order_notification_js', 'notification', array('sound' => $this->notification));
 				}
 			}
 		}
 	}
 
-	new woocommerce_order_alarm();
+	new woocommerce_notification();
 }
